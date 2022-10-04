719eea9196b88646bd7c9fb1554e1cef032e38af
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-05 01:01:16 +0300

    Add login/home/about component

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactDOM } from 'react-dom';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactDOM } from 'react-dom';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
