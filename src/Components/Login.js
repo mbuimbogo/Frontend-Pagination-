5aa49d7a44618bcb715ee9f1e09e9d242b25779e
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-05 01:07:13 +0300

    Add login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <h1>Login</h1>
+        <form>
+            <div>
+            <input type="text" name="username" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" name="password" placeholder='Password'></input>
+            </div>
+            <input type="submit" value="Submit"></input>
+        </form>
+
+        </div>
   )
 }
 
