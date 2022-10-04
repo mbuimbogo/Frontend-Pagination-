cfe5658f42ba0d704b9fa686988cb3ab1616275e
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-04 16:15:32 +0300

    Validating if data is loaded

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+const Posts = ({posts, loading}) => {
+    if(loading){
+        return <h2>Loading</h2>;
+    }
+
+  
+  return <ul className='list-group mb-4'>
+
+  </ul>
+}
+
+export default Posts
